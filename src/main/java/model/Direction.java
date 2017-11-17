31bfbf5d593465949af88047ba7a088bf797609f
charliefeng <siyu@airwallex.com>
2017-11-17 12:19:27 +1100

    add some general comment

--- src/main/java/model/Direction.java
+++ src/main/java/model/Direction.java
@@ -2,6 +2,9 @@ package main.java.model;
 
 import static main.java.model.Direction.Turn.RIGHT;
 
+/**
+ * The Direction defines the direction(Orientation) that robot can face towards, and the direction(Turn) that robot can rotate,
+ */
 public class Direction {
 
     public enum Orientation {
@@ -10,6 +13,11 @@ public class Direction {
 
         private static Orientation[] orientations= values();
 
+        /**
+         * Calculate the next Orientation after turn 90 degrees from current Orientation.
+         * @param turn
+         * @return
+         */
         Orientation turnTo(Turn turn) {
             return  orientations[(4 + this.ordinal() + (turn == RIGHT ? 1 : -1)) % 4];
         }
